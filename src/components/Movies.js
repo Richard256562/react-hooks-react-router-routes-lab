09a62697c1101271f4917b980a78ec3bb828fd48
RichardOkongo <rirachard19@gmail.com>
2023-01-22 17:46:48 +0300

    lab done

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,28 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>
+    <h1>Movies Page</h1>
+  </div>
 }
+<div>
+{movies.map((movie) => {
+  return (
+    <div key={movie.title}>
+      <h2>{movie.title}</h2>
+      <p>Time: {movie.time}</p>
+      <p>Genres: </p>
+    <ul>
+      {movie.genres.map((genre) => {
+        return (
+          <li key={genre}>{genre}</li>
+        )},
+      )},
+    </ul>
+  </div>
+)})}
+</div>
+
+
 
 export default Movies;
